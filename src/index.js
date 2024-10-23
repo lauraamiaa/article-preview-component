b193fe65ccd66142278000308d71a273e25b1bf7
lauraamiaa <miatkowski.laura@gmail.com>
2024-10-23 15:15:01 +0200

    mobile adjustments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const tooltip = document.getElementById("tooltip")
 const shareButton = document.getElementById("share-button")
-
+const shareButtonMobile = document.getElementById("share-button-mobile")
 
 function toggleShareOptions() {
     tooltip.classList.toggle("hidden")
   }
 shareButton.addEventListener("click", toggleShareOptions);
+shareButtonMobile.addEventListener("click", toggleShareOptions)
 
